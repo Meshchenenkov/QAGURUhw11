ad7afd4f96a59d83e17543a61e0ce2435d93c02d
Andrei Meshchenenkov <meschenenkovjob@gmail.com>
2025-07-05 01:15:20 +0400

    init commit

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -2,19 +2,19 @@ import allure
 from selene import have, by
 
 
-@allure.title("Successful fill form")
+@allure.title("Успешно заполняем форму")
 def test_successful(setup_browser):
     browser = setup_browser
     first_name = "Alex"
     last_name = "Egorov"
 
-    with allure.step("Open registrations form"):
+    with allure.step("Открываем форму регистрации"):
         browser.open("https://demoqa.com/automation-practice-form")
         browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
         browser.driver.execute_script("$('footer').remove()")
         browser.driver.execute_script("$('#fixedban').remove()")
 
-    with allure.step("Fill form"):
+    with allure.step("Заполняем форму"):
         browser.element("#firstName").set_value(first_name)
         browser.element("#lastName").set_value(last_name)
         browser.element("#userEmail").set_value("alex@egorov.com")
@@ -35,7 +35,7 @@ def test_successful(setup_browser):
         browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
         browser.element("#submit").click()
 
-    with allure.step("Check form results"):
+    with allure.step("Проверяем результат отправки формы"):
         browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
         # browser.element(".table-responsive").should(
         #     have.texts(first_name, last_name, "alex@egorov.com", "Some street 1"))
